2c5dd5af0104b0077826fdc0f62ae1c2fa28009d
gustavo <tavinhocassis@gmail.com>
2024-04-16 10:42:35 -0300

    trabalho de desponde

---
+++ Appdotempo/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Appdotempo;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Appdotempo/MainPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace Appdotempo;
+
+public partial class MainPage : ContentPage
+{
+	
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	
+}
+
